c5c8b739734d9897b9975c5bc8d57bcc785df528
IvanTurgenheb <dltkdwls60202@gmail.com>
2025-02-13 19:49:12 +0900

    버튼 하나로 여러개 파일 업로드하기

--- src/App.js
+++ src/App.js
@@ -1,33 +1,44 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 function App() {
-  const [image, setImage] = useState("");
+  const [images, setImages] = useState([]);
+  const formDataRef = useRef(new FormData());
 
   const uploadFile = (e) => {
     e.preventDefault();
 
-    const reader = new FileReader();
-    reader.readAsDataURL(e.target.files[0]); // readAsDataURL: 선택한 파일을 url 로 변환해주는 함수
+    let files = e.target.files;
 
-    reader.onloadend = () => {
-      // onloadend: 읽기 동작이 발생하면 성공여부와 상관없이 호출되는 이벤트 핸들러함수
-      setImage(reader.result);
-    };
+    if (!files.length) return;
+
+    const formData = new FormData();
+    let images = [];
+
+    for (let i = 0; i < files.length; i++) {
+      formData.append("images", files[i]); // 백엔드 요청용
+      images.push(URL.createObjectURL(files[i])); // 미리보기 전용
+    }
+
+    formDataRef.current = formData;
+
+    setImages(images);
   };
 
   return (
     <form>
-      <input type="file" onChange={uploadFile} />
-
+      <input type="file" multiple onChange={uploadFile} />
       <br />
-
-      <img src={image} alt="img" />
+      <div>
+        {images.map((img, idx) => (
+          <img key={idx} src={img} alt={`Uploaded ${idx}`} width={100} />
+        ))}
+      </div>
     </form>
   );
 }
 
 export default App;
 
-// 파일 하나 업로드 하기
+// 파일 하나 업로드 하기 v
 // 파일 여러개 업로드 하기
 // 파일 하나씩 여러개 업로드 하기
